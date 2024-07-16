9ab62d5eda1395394feb684b6130c1de4c653ab8
HP <praveenkumarpiyli018@gmail.com>
2024-07-16 06:55:41 -0700

    ATB7x

---
+++ src/ex_13072024/Lab007.java
@@ -0,0 +1,11 @@
+package ex_13072024;
+
+public class Lab007 {
+
+    public static void main(String[] args) {
+
+        int _123 = 243;
+        System.out.println(_123);
+
+    }
+}
---
+++ src/ex_13072024/Lab009.java
@@ -0,0 +1,12 @@
+package ex_13072024;
+
+public class Lab009 {
+    public static void main(String[] args) {
+
+        int a = 28;
+        byte b = 28;
+        System.out.println(a+b);
+
+
+    }
+}
