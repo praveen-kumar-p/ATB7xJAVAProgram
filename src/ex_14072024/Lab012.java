f7d37e3b6ab7108788a5ffd03fadc863517949f9
HP <praveenkumarpiyli018@gmail.com>
2024-07-16 09:51:43 -0700

    ATB7x

---
+++ src/ex_14072024/Lab012.java
@@ -0,0 +1,22 @@
+package ex_14072024;
+
+public class Lab012 {
+
+    public static void main(String[] args) {
+
+        //Arithmetic operator --> +,/,*,-
+
+        //Compound operator --> =+,=/,=*,=-
+
+        //Relational operator --> >,<,>=,<=, !=
+
+        //Logical Operator --> OR or and, ||, &&
+
+        //Difference Between --> = and ==
+        //= means Assign the value
+        //== means comparing [true and false]
+
+
+
+    }
+}
