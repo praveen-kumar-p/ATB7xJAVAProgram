dbf4925f17536aedd0ae7b868a5c37c671670398
HP <praveenkumarpiyli018@gmail.com>
2024-08-11 10:55:03 +0530

    06-08-2024

---
+++ src/aug_ex10082024_encapulation/Lab046.java
@@ -0,0 +1,23 @@
+package aug_ex10082024_encapulation;
+
+
+//Here we should not change the password in encapulatio
+
+public class Lab046 {
+    public static void main(String[] args) {
+        VWOLogin login = new VWOLogin("praveen", "Praveen018");
+        System.out.println(login.password);
+        login.password = "Pr@veen018";
+        System.out.println(login.password);
+    }
+
+}
+class VWOLogin{
+    public String username;
+    public String password;
+
+    public VWOLogin(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
---
+++ src/aug_ex10082024_encapulation/Lab048.java
@@ -0,0 +1,22 @@
+package aug_ex10082024_encapulation;
+
+public class Lab048 {
+
+    public static void main(String[] args) {
+
+        //This will not be allowed because "Praveen is not admin"
+        //Praveen can allowed to GET the Data not to set
+        ICICIBank Praveen = new ICICIBank("Praveen", 100);
+        System.out.println(Praveen.getName());
+        System.out.println(Praveen.getBalance());
+
+
+        //This is possible because "He is Admin"
+        //The balance will be Overrided
+        //If balance = 100, after setting it can be "5000" then the output will be "5000"
+        ICICIBank admin = new ICICIBank("admin", 1000);
+        admin.setBalance(5000, true);
+        System.out.println(admin.getName());
+        System.out.println(admin.getBalance());
+    }
+}
