70b4c2275af94529d4ea9726f9da0601df1afc83
HP <praveenkumarpiyli018@gmail.com>
2024-08-12 18:01:14 +0530

    12-08-2024 Part-2

---
+++ src/aug_ex10082024_poly/Family.java
@@ -0,0 +1,39 @@
+package aug_ex10082024_poly;
+
+public class Family {
+
+    // Encapsulation
+    private String name;
+    private String name1;
+
+    public Family(String name, String name1){
+        this.name = name;
+        this.name1 = name1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName1() {
+        return name1;
+    }
+
+    public void setName1(String name1) {
+        this.name1 = name1;
+    }
+
+
+    // From here MethodOverLoading
+    String add(String a, String b){
+        return a+b;
+    }
+
+    int add(int a, int b){
+        return a+b;
+    }
+}
---
+++ src/aug_ex10082024_poly/Lab052Family.java
@@ -0,0 +1,20 @@
+package aug_ex10082024_poly;
+
+public class Lab052Family {
+    public static void main(String[] args) {
+
+        Family f = new Family("Prakash", "Vittappa");
+        String result1 = f.add("Praveen", "Kumar");
+        int result2 = f.add(01, 01);
+        String result3 = f.add("Prakash ", "Shantha");
+
+
+        System.out.println(f.getName());
+        System.out.println(f.getName1());
+
+
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+    }
+}
