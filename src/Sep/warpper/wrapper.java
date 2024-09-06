d8a97d0191e52d9555423ad9f312d88bc278de64
0554 <praveen@ymtsindia.org>
2024-09-06 18:53:59 +0530

    praveen September

---
+++ src/Sep/warpper/wrapper.java
@@ -0,0 +1,15 @@
+package Sep.warpper;
+
+public class wrapper {
+    public static void main(String[] args) {
+
+//        int a = 10;
+//        Integer b = a;
+//        System.out.println(a);
+//        System.out.println(b);
+
+        String n = "18";
+        Integer a = Integer.valueOf(n);
+        System.out.println(a);
+    }
+}
